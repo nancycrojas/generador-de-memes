1a096f8e13cb95ad7b6ef8d99f89f4218a332755
nancycrojas <nanclarisaok@gmail.com>
2022-11-01 23:24:10 -0300

    Agrego cambiar fondo de imagen y cerrar abrir y alternar panel

--- main.js
+++ main.js
@@ -12,14 +12,24 @@ const descargarImagen = document.getElementById("img-descargar");
 const downloadBtn = document.getElementById("download-btn");
 const closeImgPanel = $(".close-image-panel");
 const closeTextPanel = $(".close-text-panel");
+const panelAsideImg = $(".panel-aside-img");
+const panelAsideTxt = $(".panel-aside-txt");
+const btnImage = $(".btn-image");
+const btnText = $(".btn-text");
+
+const contMeme = $(".image-meme");
+const inputColor = $("#input-color");
+const inputTopText = $("#input-top-text");
+const inputBottomText = $("#input-bottom-text");
 
 // Funciones
 
-//  Modo claro / modo oscuro
+//  Modo claro y modo oscuro
 btn.addEventListener("click",()=> {
 header.classList.toggle("lightMode");
 body.classList.toggle("lightMode2");
 aside.classList.toggle("lightModeAside");
+panelAsideTxt.classList.toggle("lightModeAside")
 if(header.classList.contains("lightMode")){
     btn.innerHTML = '<i class="fa-solid fa-lightbulb"></i>Modo Oscuro'
 }else{
@@ -29,14 +39,14 @@ if(header.classList.contains("lightMode")){
 
 //Eventos
 
-//Agregar  url de imagen
+// Agregar imagen con url
 url.addEventListener("input", (event) => {
   let imageUrl = event.target.value;
   imageMeme.style.backgroundImage = `url(${imageUrl})`;
 });
 
 
-//Descargar meme
+// Descargar imagen
 const descargarMeme = () => {
   domtoimage.toBlob(descargarImagen).then(function (blob) {
     saveAs(blob, "mi-meme.jpg");
@@ -45,10 +55,45 @@ const descargarMeme = () => {
 
 downloadBtn.addEventListener('click', descargarMeme);
 
-//Cerrar modal
+//Cerrar abrir y alternar panel
 closeImgPanel.addEventListener("click", ()=>{
-  aside.classList.add("hidden");
-})  
-  
+  panelAsideImg.classList.add("hidden");
+}) 
+
+btnImage.addEventListener("click", () => {
+  panelAsideImg.classList.remove("hidden");
+  panelAsideTxt.classList.add("hidden");
+});
+
+closeTextPanel.addEventListener("click", ()=>{
+  panelAsideTxt.classList.add("hidden");
+}) 
+
+btnText.addEventListener("click", () => {
+  panelAsideTxt.classList.remove("hidden");
+  panelAsideImg.classList.add("hidden");
+});
+
+
+// Cambiar color de fondo de la imagen
+inputColor.addEventListener("blur", (event) => {
+  descargarImagen.style.backgroundColor = event.target.value
+});
+
+// Editar texto superior
+inputTopText.addEventListener("input", (event)=>{
+  descargarImagen.innerText = event.target.value;
+})
+
+
+
+
+// Editar texto inferior
+inputBottomText.addEventListener("input", (event)=>{
+  descargarImagen.innerText = event.target.value;
+})
+
+
+
 
 
